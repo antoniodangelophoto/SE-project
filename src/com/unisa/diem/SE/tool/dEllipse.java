7577ab6e9e8cca936e1acc40b127550daa121508
Antonio D'Angelo <a.dangelo83@studenti.unisa.it>
2022-11-25 19:06:46 +0100

    ellipse in work

--- src/com/unisa/diem/SE/tool/dEllipse.java
+++ src/com/unisa/diem/SE/tool/dEllipse.java
@@ -6,19 +6,19 @@ package com.unisa.diem.SE.tool;
 
 import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.canvas.Canvas;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.layout.Pane;
 
 /**
  *
  * @author antonio.dangelo.photo
  */
-public class Ellipse extends Shapes {
+public class dEllipse extends Shapes {
     
     private double hRadius;
     private double vRadius;
     
-    public Ellipse(){
+    public dEllipse(){
         
     }
 
@@ -38,17 +38,20 @@ public class Ellipse extends Shapes {
         this.vRadius = vRadius;
     }
     
-    public Ellipse(Point2D startPos, Point2D endPos, Color strockColor){
+    public dEllipse(Point2D startPos, Point2D endPos, Color strockColor){
         super(startPos, endPos, strockColor);
         hRadius = Math.abs(startPos.getX() - endPos.getX())/2;
         vRadius = Math.abs(startPos.getY() - endPos.getY())/2;
     }
-    /*@Override
+    @Override
     public void draw(Pane Pane){
+        Ellipse ellipse=new Ellipse();
+        
+        
         GraphicsContext gc = Pane.getGraphicsContext2D();
         gc.setStroke(super.getColor());
         gc.strokeOval(super.getTopLeft().getX(), super.getTopLeft().getY(), hRadius*2, vRadius*2);
         gc.setFill(super.getFillColor());
         gc.fillOval(super.getTopLeft().getX(), super.getTopLeft().getY(), hRadius*2, vRadius*2);
-    }*/
+    }
 }
