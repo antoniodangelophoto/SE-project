af294e4f5a029166d9ab0178fe0610ca2db46b4a
fracarr13 <f.carrozza2@studenti.unisa.it>
2022-12-01 18:27:27 +0100

    Command

---
+++ src/com/unisa/diem/SE/tool/CommandExecutor.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.unisa.diem.SE.tool;
+
+import java.util.*;
+
+/**
+ *
+ * @author Francesco
+ */
+public class CommandExecutor {
+    
+    private Deque<Command> stack;
+    public CommandExecutor(){
+        stack = new ArrayDeque<>();
+    }
+    public void execute(Command command){
+        stack.addLast(command);
+        command.execute();
+    }
+    public void undoLast(){
+        Command last = stack.removeLast();
+        last.undo();
+    }
+    
+    
+}
