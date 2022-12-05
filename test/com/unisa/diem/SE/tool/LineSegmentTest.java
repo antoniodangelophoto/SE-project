304705d996e190719dcf34a5857d16a453d578ad
Antonio D'Angelo <a.dangelo83@studenti.unisa.it>
2022-12-05 09:29:34 +0100

    command #60
    Fine Sprint 2

--- test/com/unisa/diem/SE/tool/LineSegmentTest.java
+++ test/com/unisa/diem/SE/tool/LineSegmentTest.java
@@ -1,3 +1,5 @@
+package com.unisa.diem.SE.tool;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,26 +9,26 @@
  *
  * @author GBCuomo
  */
-import com.unisa.diem.SE.tool.dEllipse;
+import com.unisa.diem.SE.tool.LineSegment;
 import com.unisa.diem.SE.tool.Shapes;
 import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 
-public class dEllipseTest {
+public class LineSegmentTest {
 
     @Test
     public void testToString(){
 
         Point2D start = new Point2D(75.0, 80.0);
         Point2D end = new Point2D(80.0, 92.0);
-        dEllipse e = new dEllipse(start, end, Color.valueOf("0xffff0000"),Color.valueOf("0x00ffff00"));
-        assertEquals(e.toString(),"Ellipse,75.0,80.0,80.0,92.0,0xffff0000,0x00ffff00\n");
+        LineSegment l = new LineSegment(start, end, Color.valueOf("0xffff0000"));
+        assertEquals(l.toString(),"Line,75.0,80.0,80.0,92.0,0xffff0000\n");
 
     }
 
--- test/com/unisa/diem/SE/tool/ShapesTest.java
+++ test/com/unisa/diem/SE/tool/ShapesTest.java
@@ -1,3 +1,5 @@
+package com.unisa.diem.SE.tool;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,26 +9,27 @@
  *
  * @author GBCuomo
  */
-import com.unisa.diem.SE.tool.Rect;
+
 import com.unisa.diem.SE.tool.Shapes;
+import com.unisa.diem.SE.tool.dEllipse;
 import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 
-public class RectTest {
+public class ShapesTest {
 
     @Test
     public void testToString(){
 
         Point2D start = new Point2D(75.0, 80.0);
         Point2D end = new Point2D(80.0, 92.0);
-        Rect r = new Rect(start, end, Color.valueOf("0xffff0000"),Color.valueOf("0x00ffff00"));
-        assertEquals(r.toString(),"Rectangle,75.0,80.0,80.0,92.0,0xffff0000,0x00ffff00\n");
+        Shapes e = new dEllipse(start, end, Color.valueOf("0xffff0000"),Color.valueOf("0x00ffff00"));
+        assertEquals(e.toString(),"Ellipse,75.0,80.0,80.0,92.0,0xffff0000,0x00ffff00\n");
 
     }
 
